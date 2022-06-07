9b6240103516e3b32a01ec44329b40df8b9d9afe
malamehta <malamehta370@gmail.com>
2022-06-07 14:04:30 +0545

    file added

---
+++ moderncss/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./Public/index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'light-orange':'#FF9C00',
+        'grey-color':'#A9B2BC',
+        'darkest-blue':'#011021',
+        'dark-blue-color':'#072753',
+        'blue-color':'#28384E',
+        'light-blue-color':'#6DBBD9',
+      
+      },
+      fontFamily:{
+        'Helvetica':'Helvetica Neue',
+      },
+    },
+  },
+  plugins: [],
+}
